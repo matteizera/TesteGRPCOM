8c3e2de71740e3cc6be2378aef85f44200607462
matteizera <lucasmattei2000@gmail.com>
2021-04-03 02:08:28 -0300

    Adicionando react

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import cors from 'cors'
 import 'express-async-errors'
 import routes from './routes'
-
+const path = require('path' ) ;
 const app = express()
-app.use(cors())
 
+const port = process.env.PORT || 3333
+
+app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.listen(3333)
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')));
+app.listen(port)
